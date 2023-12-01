60b1bcc99322bc501142367cebe8b325974dde98
Brian Kheng <briankheng63@gmail.com>
2023-12-01 15:35:09 +0700

    feat: LockManagerB

--- txn/lock_manager.cc
+++ txn/lock_manager.cc
@@ -135,25 +135,125 @@ LockManagerB::LockManagerB(deque<Txn*>* ready_txns) {
 }
 
 bool LockManagerB::WriteLock(Txn* txn, const Key& key) {
-  //
-  // Implement this method!
-  return true;
+  if (lock_table_.find(key) == lock_table_.end()) {
+    deque<LockRequest> *new_deque = new deque<LockRequest>();
+    new_deque->push_back(LockRequest(EXCLUSIVE, txn));
+    lock_table_.insert(pair<Key, deque<LockRequest>*>(key, new_deque));
+
+    // signal instant lock access
+    txn_waits_[txn] = 0;
+    return true;
+  } else {
+    deque<LockRequest> *lock_deq = lock_table_[key];
+    lock_deq->push_back(LockRequest(EXCLUSIVE, txn));
+
+    // if this is the only lock request
+    if (lock_deq->size() == 1) {
+      // signal instant lock access
+      txn_waits_[txn] = 0;
+      return true;
+    } else {
+      // signal wait
+      txn_waits_[txn]++;
+      return false;
+    }
+  }
 }
 
 bool LockManagerB::ReadLock(Txn* txn, const Key& key) {
-  //
-  // Implement this method!
-  return true;
+  // if deque not found, create new deque and add Lock Request
+  if (lock_table_.find(key) == lock_table_.end()) {
+    deque<LockRequest> *new_deque = new deque<LockRequest>();
+    new_deque->push_back(LockRequest(SHARED, txn));
+    lock_table_.insert(pair<Key, deque<LockRequest>*>(key, new_deque));
+
+    // signal instant lock access
+    txn_waits_[txn] = 0;
+    return true;
+  } 
+  // if deque found
+  else {
+    deque<LockRequest> *lock_deq = lock_table_[key];
+    lock_deq->push_back(LockRequest(SHARED, txn));
+
+    // if this is the only lock request
+    if (lock_deq->size() == 1) {
+      // signal instant lock access
+      txn_waits_[txn] = 0;
+      return true;
+    }
+    // if there are other lock requests
+    else {
+      // Check if there is a write lock request
+      deque<LockRequest>::iterator it = lock_deq->begin();
+      for (; it != lock_deq->end(); it++) {
+        if (it->mode_ == EXCLUSIVE) {
+          // signal wait
+          txn_waits_[txn]++;
+          return false;
+        }
+      }
+
+      // signal instant lock access (Only shared locks exist)
+      txn_waits_[txn] = 0;
+      return true;
+    }
+  }
 }
 
 void LockManagerB::Release(Txn* txn, const Key& key) {
-  //
-  // Implement this method!
+  deque<LockRequest> *lock_deq = lock_table_[key];
+
+  deque<LockRequest>::iterator it = lock_deq->begin();
+  for (; it != lock_deq->end(); it++) {
+    if (it->txn_ == txn) {
+      lock_deq->erase(it);
+      break;
+    }
+  }
+  
+  vector<Txn*> owners; Status(key, &owners);
+
+  for (auto owner : owners) {
+    auto txn = txn_waits_.find(owner);
+    if (txn != txn_waits_.end() && --(txn->second) == 0) {
+      ready_txns_->push_back(owner);
+      txn_waits_.erase(txn);
+    }
+  }
 }
 
 LockMode LockManagerB::Status(const Key& key, vector<Txn*>* owners) {
-  //
-  // Implement this method!
-  return UNLOCKED;
-}
+  owners->clear();
 
+  // if deque not found, return UNLOCKED
+  if (lock_table_.find(key) == lock_table_.end()) {
+    return UNLOCKED;
+  }
+  // if deque found
+  else {
+    deque<LockRequest> *lock_deq = lock_table_[key];
+
+    // if deque is empty, return UNLOCKED
+    if (lock_deq->size() == 0) {
+      return UNLOCKED;
+    }
+    // if deque is not empty
+    else {
+      deque<LockRequest>::iterator it = lock_deq->begin();
+
+      // Check if current lock is exclusive
+      if (it->mode_ == EXCLUSIVE) {
+        owners->push_back(it->txn_);
+        return EXCLUSIVE;
+      }
+      // Return shared lock
+      else{
+        for (; it != lock_deq->end() && it->mode_ != EXCLUSIVE; it++) {
+          owners->push_back(it->txn_);
+        }
+        return SHARED;
+      }
+    }
+  }
+}
